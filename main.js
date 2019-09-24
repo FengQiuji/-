875dacd251e52c3412f63be57f1631fe1cf3fcf4
FengQiuji <524520775@qq.com>
2019-09-24 20:56:11 +0800

    安装axios

---
+++ main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import Component from './components' // 引入插件对象
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将地址的常态值设置给baseUrl
+Vue.prototype.$axios = axios // 将axios共享给所有Vue或者组件实例使用
+Vue.config.productionTip = false
+Vue.use(ElementUI) // 相当于全局注册elementUI  Vue.use => 干了什么?
+Vue.use(Component) // 注册插件
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '../public/index.less'
+import axios from 'axios'
+//  引入elementui
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+Vue.use(ElementUI) //     全局注册elementui
 
 new Vue({
   router,
