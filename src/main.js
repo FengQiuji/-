73c7964e72e656e2bcbf1a419293aab1465bab61
FengQiuji <524520775@qq.com>
2019-09-24 21:09:37 +0800

    test

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '../public/index.less'
 import axios from 'axios'
+// 测试使用
 //  引入elementui
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
